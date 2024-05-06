f439a0897ee892001460dff79d41c78043fa8bd5
Marco Di Gennaro <m.di.gennaro@outlook.com>
2024-05-06 13:12:35 +0200

    add figure

--- optimization/config.py
+++ optimization/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # Get the base directory (directory where this file resides)
-BASE_DIR = Path(__file__).parent.parent
+BASE_DIR = Path(__file__).parent
 
 # Paths to various resources
 IMAGES_DIR = BASE_DIR / 'images'
